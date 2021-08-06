0c69c9de38b3c690865d3ea2d9fcc9a80b3f6ea8
Patrick Lake <plake492@gmail.com>
2021-08-06 16:42:02 -0400

    Add route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 
 const Home = () => import('../views/Home.vue')
 const Login = () => import('../views/Login.vue')
@@ -11,17 +12,20 @@ const routes = [
   {
     path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: { auth: true }
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: { auth: false }
   },
   {
     path: '/signup',
     name: 'signup',
-    component: Signup
+    component: Signup,
+    meta: { auth: false }
   },
   {
     path: '*',
@@ -33,6 +37,25 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  // Check if route requires auth
+  const auth = to.matched.some(record => record.meta.auth)
+
+  // Check if user is authenticated vie vuex store
+  const authenticated = store.state.auth.authenticated
+
+  if (auth && !authenticated) {
+    return next({ name: 'login' })
+  }
+
+  // restrict authenticated user from going to nonauthenticated routes
+  if (authenticated && !auth) {
+    return next(from.path)
+  }
+
+  return next()
 })
 
 export default router
