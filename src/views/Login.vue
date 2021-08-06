c12f276c87e3307b9b4bd8b51b09ef17e89b2c00
Patrick Lake <plake492@gmail.com>
2021-08-06 11:47:55 -0400

    Create basic login flow

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,54 @@
 <template>
-  <div>
+  <section>
     <h1>Login</h1>
-  </div>
+    <form @submit.prevent="login">
+      <div>
+        <label>email</label>
+        <input type="email" v-model="user.email" />
+      </div>
+      <div>
+        <label>password</label>
+        <input type="password" v-model="user.password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    <div>
+      <small>
+        Don't have an account?
+        <span>
+          <router-link style="color: dodgerblue" :to="{ name: 'signup' }">
+            Creat one here
+          </router-link>
+        </span>
+      </small>
+    </div>
+    <div v-if="error" style="color: red">{{ error }}</div>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Login',
-  methods: {}
+  data() {
+    return {
+      user: {
+        email: '',
+        password: ''
+      },
+      error: ''
+    }
+  },
+  methods: {
+    // Expect login to be asynchronous
+    async login() {
+      try {
+        await this.$store.dispatch('auth/login', { user: this.user })
+        this.$router.push({ name: 'home' })
+      } catch (err) {
+        //Set error message
+        this.error = err.message
+      }
+    }
+  }
 }
 </script>
