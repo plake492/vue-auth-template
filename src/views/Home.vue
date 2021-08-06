ebab67282f05ec44d8bb84eb1ed73b4567dcbaa3
Patrick Lake <plake492@gmail.com>
2021-08-06 16:42:29 -0400

    Display user on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
     <h1>Home Page</h1>
-    <p>{{ user }}</p>
+    <p>Email: {{ user }}</p>
+    <p>Is Authenticated: {{ authenticated }}</p>
   </section>
 </template>
 
@@ -11,7 +12,17 @@ export default {
   computed: {
     user() {
       //Get reactive User Object
-      return this.$store.state.auth.user
+      return this.$store.state.auth.user.email
+    },
+    authenticated() {
+      //Get Auth status for route guard
+      return this.$store.state.auth.authenticated
+    }
+  },
+  mounted() {
+    // Router guard to kick out none authenticated users
+    if (!this.authenticated) {
+      this.$router.push({ name: 'login' })
     }
   }
 }
