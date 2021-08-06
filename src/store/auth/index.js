fe8fc0db950f8e622b4b12d064e93d65ee9caf13
Patrick Lake <plake492@gmail.com>
2021-08-06 11:14:46 -0400

    Set up auth store

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -1,7 +1,40 @@
 export const auth = {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
-  actions: {},
+  state: () => ({
+    user: null,
+    authenticated: false,
+    loading: false
+  }),
+
+  mutations: {
+    SET_USER (state, user) {
+      state.user = user
+    },
+    SET_AUTHENTICATED (state, authenticated) {
+      state.authenticated = authenticated
+    },
+    SET_LOADIN (state, loading) {
+      state.loading = loading
+    }
+  },
+
+  actions: {
+    setUser ({ commit }, { user } /** Destructuring params */) {
+      commit('SET_USER', user)
+    },
+    setAuthenticated ({ commit }, { authenticated }) {
+      commit('SET_AUTHENTICATED', authenticated)
+    },
+    setLoading ({ commit }, { loading }) {
+      commit('SET_LOADING', loading)
+    },
+    login ({ commit }, { user, authenticated }) {
+      commit('SET_AUTHENTICATED', authenticated)
+      commit('SET_USER', user)
+    },
+    signup (_, { user }) {
+
+    }
+  },
   getters: {}
 }
