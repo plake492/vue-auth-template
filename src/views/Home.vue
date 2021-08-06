c8e9162ea70525bc783f00b85c06ca57a77272bc
Patrick Lake <plake492@gmail.com>
2021-08-06 11:06:23 -0400

    Formate code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Home Page</h1>
+    <p>{{ user }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'Home',
+  computed: {
+    user() {
+      //Get reactive User Object
+      return this.$store.state.auth.user
+    }
+  }
+}
 </script>
